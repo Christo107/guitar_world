57755110c4b76f3b1677bad72233115c2d713af1
Christo107 <christopher.odwyer82@gmail.com>
2023-08-31 15:29:55 +0000

    Deploy to heroku initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn guitar_world.wsgi:application
--- guitar_world/settings.py
+++ guitar_world/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-%g6ml$dwa1o@h7poadxyh044!ml&#0ycxae!on2acpd$ro$(m)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-christo107-guitarworld-ch5h12k9b9r.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['guitar-world.herokuapp.com', 'localhost'
+                 '8000-christo107-guitarworld-ch5h12k9b9r.ws-eu104.gitpod.io']
 
 
 # Application definition
@@ -120,12 +121,17 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-christo107-guitarworld-ch5h12k9b9r.ws-eu10
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
