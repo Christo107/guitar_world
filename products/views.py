b6ba72bf4c2006c6d98c399945792aa2f7511002
Christo107 <christopher.odwyer82@gmail.com>
2023-09-08 17:00:31 +0000

    Update styling on product list

--- products/views.py
+++ products/views.py
@@ -87,6 +87,7 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
 @login_required
 def add_product(request):
     """ Add a product to the store """
