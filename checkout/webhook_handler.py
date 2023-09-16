85604408cdab19e995953fbae27a517d86a6d071
Christo107 <christopher.odwyer82@gmail.com>
2023-09-16 14:20:09 +0000

    Add placeholders to contact us page

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -75,8 +75,10 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_street_address1 = (shipping_details
+                                                   .address.line1)
+                profile.default_street_address2 = (shipping_details.
+                                                   address.line2)
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
@@ -107,7 +109,7 @@ class StripeWH_Handler:
             self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
-                status=200)
+                        status=200)
         else:
             order = None
             try:
--- contact/forms.py
+++ contact/forms.py
@@ -20,3 +20,13 @@ class ContactUsForm(forms.ModelForm):
             'subject': 'Subject',
             'message': 'Message',
         }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
