229bb0a28d97f35acb29e8c731c825316058a619
Christo107 <christopher.odwyer82@gmail.com>
2023-09-16 11:42:35 +0000

    Tidy code remvoing unnecessary links

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -57,10 +57,9 @@ class StripeWH_Handler:
             intent.latest_charge
         )
 
-        billing_details = stripe_charge.billing_details # updated
+        billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2) # updated
-
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
             if value == "":
