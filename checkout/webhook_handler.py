3958c2701dab0c7dfaa62e924878eed2718f006d
Christo107 <christopher.odwyer82@gmail.com>
2023-11-02 16:53:40 +0000

    Try fix for stripe bug

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -80,22 +80,22 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
-        # Update profile information if save_info was checked
-        profile = None
-        username = intent.metadata.username
-        if username != 'AnonymousUser':
-            profile = UserProfile.objects.get(user__username=username)
-            if save_info:
-                profile.default_phone_number = shipping_details.phone
-                profile.default_country = shipping_details.address.country
-                profile.default_postcode = shipping_details.address.postal_code
-                profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = (shipping_details
-                                                   .address.line1)
-                profile.default_street_address2 = (shipping_details.
-                                                   address.line2)
-                profile.default_county = shipping_details.address.state
-                profile.save()
+        # # Update profile information if save_info was checked
+        # profile = None
+        # username = intent.metadata.username
+        # if username != 'AnonymousUser':
+        #     profile = UserProfile.objects.get(user__username=username)
+        #     if save_info:
+        #         profile.default_phone_number = shipping_details.phone
+        #         profile.default_country = shipping_details.address.country
+        #         profile.default_postcode = shipping_details.address.postal_code
+        #         profile.default_town_or_city = shipping_details.address.city
+        #         profile.default_street_address1 = (shipping_details
+        #                                            .address.line1)
+        #         profile.default_street_address2 = (shipping_details.
+        #                                            address.line2)
+        #         profile.default_county = shipping_details.address.state
+        #         profile.save()
 
         order_exists = False
         attempt = 1
@@ -123,8 +123,7 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} |\
-                    SUCCESS: Verified order already in database',
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                         status=200)
         else:
             order = None
@@ -170,8 +169,7 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | \
-                SUCCESS: Created order in webhook',
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
